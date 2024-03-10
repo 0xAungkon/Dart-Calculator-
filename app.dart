160875620b05c52bd6184934c1fd19e9a87a31d7
onlycoder000 <onlycoder000@gmail.com>
2024-03-10 15:38:37 +0600

    auto commit

--- app.dart
+++ app.dart
@@ -2,10 +2,7 @@ import 'dart:io';
 
 
 void main() async {
-  stdout.write('Enter your name: ');
-
-  // Read user input
-  String name = stdin.readLineSync();
+  String name=stdin.readLineSync();
 
   // Print a greeting with the user's name
   print('Hello, $name!');
