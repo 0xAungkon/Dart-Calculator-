e929f26b1e8a161277eb68702d38734c1c13fe07
onlycoder000 <onlycoder000@gmail.com>
2024-03-10 15:49:55 +0600

    auto commit

--- calculator.dart
+++ calculator.dart
@@ -5,15 +5,15 @@ void main() async {
     
 
     while(true){
+        stdout.write('Enter a number: ');
         String? number1=stdin.readLineSync();
         try{
-        int number=int.parse(number1);
-    }
-    catch (FormatException) {   
-        print("it's not a number, Try Again")
-        continue
-    }
-
+            int number=int.parse(number1);
+        }
+        catch (FormatException) {   
+            print("it's not a number, Try Again.")
+            continue
+        }
     }
     String? number2=stdin.readLineSync();
     
