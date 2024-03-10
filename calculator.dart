f0235d54bbfdfc16babeb0d81638b1b67da158bd
onlycoder000 <onlycoder000@gmail.com>
2024-03-10 16:18:45 +0600

    auto commit

--- calculator.dart
+++ calculator.dart
@@ -20,43 +20,10 @@ int asking_number(String prompt) {
 void main() async {
     
     int? number2_int=asking_number('Enter 2nd number: ');
-    int? oparator_int;
+    print('\nCalcualation: \n 1.Addition \n 2.Multiplication \n 3.Divider \n 4.Subtraction');
+    int? oparator_int=asking_number('What You Want To Do: ');
     int? number1_int=asking_number('Enter a number: ');
     
-    // while(true){
-    //     print('\nCalcualation: \n 1.Addition \n 2.Multiplication \n 3.Divider \n 4.Subtraction');
-    //     stdout.write('What You Want To Do? ');
-    //     String? oparator_=stdin.readLineSync();
-    //     try{
-    //         int oparator_int=int.parse(oparator_ !);
-    //         if(oparator_int>0 && oparator_int<5){
-    //             break;
-    //         }
-    //         else{
-    //             print("Invalid Choise.");
-
-    //         }
-    //     }
-    //     catch (FormatException) {   
-    //         print("it's not a number, Try Again.");
-    //         continue;
-    //     }
-
-    // }
-
-
-    // while(true){
-    //     stdout.write('Enter a 2nd number: ');
-    //     String? number2=stdin.readLineSync();
-    //     try{
-    //         int number2_int=int.parse(number2 !);
-    //         break;
-    //     }
-    //     catch (FormatException) {   
-    //         print("it's not a number, Try Again.");
-    //         continue;
-    //     }
-    // }
 
 
 }
