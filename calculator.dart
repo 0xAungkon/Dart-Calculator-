f257f0fa8b2095d3bc70a059601093e7954d1353
onlycoder000 <onlycoder000@gmail.com>
2024-03-10 16:05:22 +0600

    auto commit

--- calculator.dart
+++ calculator.dart
@@ -19,9 +19,17 @@ void main() async {
         print('\nCalcualation: \n 1.Addition \n 2.Multiplication \n 3.Divider \n 4.Subtraction');
         stdout.write('What You Want To Do? ');
         String? oparator_=stdin.readLineSync();
-        if(){
-            
+        try{
+            int number1_int=int.parse(number1 !);
+            if(number1_int){
+
+            }
         }
+        catch (FormatException) {   
+            print("it's not a number, Try Again.");
+            continue;
+        }
+
     }
 
 
