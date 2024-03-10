6e670e34db0772cfc59b63196e090ccea17dd12e
onlycoder000 <onlycoder000@gmail.com>
2024-03-10 15:48:54 +0600

    auto commit

--- calculator.dart
+++ calculator.dart
@@ -2,17 +2,21 @@ import 'dart:io';
 
 
 void main() async {
-    String? number1=stdin.readLineSync();
-
     
-    String? number2=stdin.readLineSync();
-    try{
+
+    while(true){
+        String? number1=stdin.readLineSync();
+        try{
         int number=int.parse(number1);
     }
-    catch (FormatException) {
-        print("it's not a number")
+    catch (FormatException) {   
+        print("it's not a number, Try Again")
         continue
     }
+
+    }
+    String? number2=stdin.readLineSync();
+    
 
     // Print a greeting with the user's name
     print('Hello, $name!');
